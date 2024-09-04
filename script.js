8381d07035316c54b57cdd84a57661ce96fa4235
Akeab-tame <tameneakeab@gmail.com>
2024-09-03 20:20:13 -1200

    first commit for the calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+//calcualtoer
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Sintefic Error";
+    }
+}
